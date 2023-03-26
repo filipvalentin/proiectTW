df62a17b0de7e6601793432a12d6a8c2c05e7283
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-03-26 19:01:02 +0300

    Initial commit
     login and register kind of done, index needs

---
+++ script/register.js
@@ -0,0 +1,9 @@
+// const form = document.querySelector("register");
+// eInput = form.querySelector("input");
+// text = form.querySelector("emailcheck");
+
+// form.addEventListener("submit", (e)=>{
+// 	e.preventDefault();
+// 	let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+// 	form.classList.add("error")
+// })
