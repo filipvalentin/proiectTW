68ad838f9dfe42bca6533a3acd9c2304ba7430df
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-06-14 17:08:43 +0300

    dynamic information

---
+++ script/dynamicInformation.js
@@ -0,0 +1,126 @@
+var http = new XMLHttpRequest();
+http.open("GET", 'getInformation.php', true);
+http.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+http.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("JWT"));
+http.onreadystatechange = function () {
+    //Call a function when the state changes.
+    if (http.readyState == 4 && http.status == 200) {
+        var information = JSON.parse(http.responseText);
+        console.log(information);
+
+        //adauga username ul in informatii
+        var username = information.username;
+        console.log(username);
+        var mainFrame = document.getElementById("main-frame");
+        var usernameDiv = document.createElement("div");
+        usernameDiv.classList.add("username");
+        usernameDiv.textContent = username;
+        mainFrame.appendChild(usernameDiv);
+
+        //
+
+        var nameUser = information.entire_name;
+        console.log(nameUser);
+        var mainFrame = document.getElementById("information");
+        var nameUserDiv = document.createElement("div");
+        nameUserDiv.classList.add("user-info");
+        var aux1 = document.createElement("p");
+        aux1.innerHTML = "Name: "
+        nameUserDiv.appendChild(aux1);
+        var aux2 = document.createElement("p");
+        aux2.innerHTML = nameUser;
+        nameUserDiv.appendChild(aux2);
+        mainFrame.appendChild(nameUserDiv);
+
+        //
+
+        var email = information.email;
+        console.log(email);
+        var emailDiv = document.createElement("div");
+        emailDiv.classList.add("user-info");
+        var aux1 = document.createElement("p");
+        aux1.innerHTML = "Email: "
+        emailDiv.appendChild(aux1);
+        var aux2 = document.createElement("p");
+        aux2.innerHTML = email;
+        emailDiv.appendChild(aux2);
+        mainFrame.appendChild(emailDiv);
+
+        //
+
+        var creation_date = information.creation_date;
+        var creationDiv = document.createElement("div");
+        creationDiv.classList.add("user-info");
+        var aux1 = document.createElement("p");
+        aux1.innerHTML = "Here since: "
+        creationDiv.appendChild(aux1);
+        var aux2 = document.createElement("p");
+        aux2.innerHTML = creation_date;
+        creationDiv.appendChild(aux2);
+        mainFrame.appendChild(creationDiv);
+
+        //
+
+        if (information.display_gender == '1') {
+            var gender = information.gender;
+            var genderDiv = document.createElement("div");
+            genderDiv.classList.add("user-info");
+            var aux1 = document.createElement("p");
+            aux1.innerHTML = "Gender: "
+            genderDiv.appendChild(aux1);
+            var aux2 = document.createElement("p");
+            aux2.innerHTML = gender;
+            genderDiv.appendChild(aux2);
+            mainFrame.appendChild(genderDiv);
+        }
+
+        //
+
+        if (information.display_birthdate == '1') {
+            var birthday = information.birth_date;
+            var birthDiv = document.createElement("div");
+            birthDiv.classList.add("user-info");
+            var aux1 = document.createElement("p");
+            aux1.innerHTML = "My birthday : "
+            birthDiv.appendChild(aux1);
+            var aux2 = document.createElement("p");
+            aux2.innerHTML = birthday;
+            birthDiv.appendChild(aux2);
+            mainFrame.appendChild(birthDiv);
+        }
+
+        //
+
+        if (information.display_highschool == '1') {
+            var high_school = information.high_school;
+            var schoolDiv = document.createElement("div");
+            schoolDiv.classList.add("user-info");
+            var aux1 = document.createElement("p");
+            aux1.innerHTML = "I'm studying at: "
+            schoolDiv.appendChild(aux1);
+            var aux2 = document.createElement("p");
+            aux2.innerHTML = high_school;
+            schoolDiv.appendChild(aux2);
+            mainFrame.appendChild(schoolDiv);
+        }
+
+        //
+
+        var aboutMe = information.about_me;
+        var aboutDiv = document.createElement("div");
+        aboutDiv.classList.add("aboutme");
+        var aux1 = document.createElement("p");
+        aux1.innerHTML = "About me"
+        aboutDiv.appendChild(aux1);
+        var aux2 = document.createElement("p");
+        aux2.innerHTML = aboutMe;
+        aboutDiv.appendChild(aux2);
+        mainFrame.appendChild(aboutDiv);
+
+    }
+    if (http.readyState == 4 && http.status == 401) {
+        console.log('au')
+        window.location.assign("unauthorized.html");
+    }
+}
+http.send();
---
+++ src/getInformation.php
@@ -0,0 +1,47 @@
+<?php
+
+include("authorization_logic.php");
+
+$decoded = getDecodedJWT();
+
+if($decoded == null){
+	http_response_code(401);
+	die();
+}
+
+$decoded_array = (array) $decoded;
+
+$user_id = $decoded_array['sub'];
+
+
+$db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stm = $db->prepare("SELECT users.username, users.email, users.creation_date, info_users.entire_name, info_users.gender, info_users.high_school, info_users.birth_date, info_users.about_me, info_users.display_birthdate, info_users.display_gender, info_users.display_highschool FROM users JOIN info_users on users.user_id = ? AND users.user_id=info_users.id_user");
+$res = $stm->execute([$user_id]);
+$rows = $stm->fetchAll(PDO::FETCH_NUM); //passwd
+
+if(empty($rows)){ // nu s-a gasit emailul
+	http_response_code(401);
+	// echo "ENF"; //email not found
+	return;
+}
+
+$user = array(
+    'username' => $rows[0][0],
+    'email' => $rows[0][1],
+    'creation_date' => $rows[0][2],
+    'entire_name' => $rows[0][3],
+    'gender' => $rows[0][4],
+    'high_school' => $rows[0][5],
+    'birth_date' => $rows[0][6],
+    'about_me' => $rows[0][7],
+    'display_birthdate' => $rows[0][8],
+    'display_gender' => $rows[0][9],
+    'display_highschool' => $rows[0][10]
+);
+
+header('Content-Type: application/json');
+echo json_encode($user);
+
