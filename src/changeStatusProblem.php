92c0c444145a5ba8f4cfa7c67c9fa76e5a64f4f1
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-06-16 00:39:00 +0300

    uitat pus data in bd

--- src/changeStatusProblem.php
+++ src/changeStatusProblem.php
@@ -11,12 +11,13 @@ $db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stm = $db->prepare("UPDATE teacher_problems SET status = ?  WHERE id = ?");
+$stm = $db->prepare("UPDATE teacher_problems SET status = ?, status_update_date = NOW() WHERE id = ?");
 if($status == '1'){
     $stm->bindValue(1, 'VERIFIED');
 }else{
     $stm->bindValue(1, 'REJECTED');
 }
+
 $stm->bindValue(2, $idProblem);
 
 
