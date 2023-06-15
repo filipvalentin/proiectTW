ad270bd097b745edc131170d323cdc4a67c58ef7
Grajdeanu Alexandru-Cristian <100189515+grajdeanualex@users.noreply.github.com>
2023-06-15 21:41:09 +0300

    95% realizat admin user

---
+++ src/getInfoAdmin.php
@@ -0,0 +1,31 @@
+<?php
+$user_id = $_GET['id'];
+
+
+$db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stm = $db->prepare("SELECT users.username, users.email, users.role, info_users.entire_name, info_users.gender, info_users.high_school, info_users.about_me FROM users JOIN info_users on users.user_id = ? AND users.user_id=info_users.id_user");
+$res = $stm->execute([$user_id]);
+$rows = $stm->fetchAll(PDO::FETCH_NUM); //passwd
+
+if(empty($rows)){ // nu s-a gasit emailul
+	http_response_code(401);
+	// echo "ENF"; //email not found
+	return;
+}
+
+$user = array(
+    'username' => $rows[0][0],
+    'email' => $rows[0][1],
+    'role' => $rows[0][2],
+    'entire_name' => $rows[0][3],
+    'gender' => $rows[0][4],
+    'high_school' => $rows[0][5],
+    'about_me' => $rows[0][6],
+);
+
+header('Content-Type: application/json');
+echo json_encode($user);
+
