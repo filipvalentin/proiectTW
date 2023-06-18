ae2aa49e73cebfb3e1caa6810977ea8174475866
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-06-18 12:15:14 +0300

    Update get_problem.php

--- api/get_problem.php
+++ api/get_problem.php
@@ -4,13 +4,18 @@ $db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+if (!isset($_GET["id"])) {
+	http_response_code(400);
+	die();
+}
+
 $problem_id = $_GET["id"];
 
 $stm = $db->prepare("SELECT id, title, tags, difficulty, description, status, post_date FROM teacher_problems WHERE id = ?");
 $res = $stm->execute([$problem_id]);
 $results = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-if(empty($results)){
+if (empty($results)) {
 	http_response_code(404);
 	die();
 }
