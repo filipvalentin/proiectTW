55e2b1ac9d3621e3ea670959ab4c283011fc8a2b
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-06-18 12:14:26 +0300

    added api

---
+++ api/get_all_problems.php
@@ -0,0 +1,14 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stm = $db->prepare("SELECT id, title, tags, difficulty, description, status, post_date FROM teacher_problems");
+$res = $stm->execute();
+$results = $stm->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($results);
+
+http_response_code(200);
+
+echo $json;
---
+++ api/get_problem.php
@@ -0,0 +1,22 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$problem_id = $_GET["id"];
+
+$stm = $db->prepare("SELECT id, title, tags, difficulty, description, status, post_date FROM teacher_problems WHERE id = ?");
+$res = $stm->execute([$problem_id]);
+$results = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+if(empty($results)){
+	http_response_code(404);
+	die();
+}
+
+$json = json_encode($results);
+
+http_response_code(200);
+
+echo $json;
