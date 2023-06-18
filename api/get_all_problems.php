77bff02ba281451029c9cc0bb82a55da5da3e0bf
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-06-18 13:31:30 +0300

    api checks added

--- api/get_all_problems.php
+++ api/get_all_problems.php
@@ -1,5 +1,10 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+	http_response_code(400);
+	die();
+}
+
 $db = new PDO("mysql:host=localhost;dbname=project", 'root', '');
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
